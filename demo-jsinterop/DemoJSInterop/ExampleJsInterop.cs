1a254379a7571dd595d83fd59397f25bcbdc9f9a
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2019-05-01 18:35:08 -0300

    JSInterop demo

---
+++ demo-jsinterop/DemoJSInterop/ExampleJsInterop.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+public class ExampleJsInterop
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public ExampleJsInterop(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public Task CallHelloHelperSayHello(string name)
+    {
+        return _jsRuntime.InvokeAsync<object>(
+            "exampleJsFunctions.sayHello",
+            new DotNetObjectRef(new HelloHelper(name)));
+    }
+
+    public Task<string> CallHelloHelperShowPrompt()
+    {
+        return _jsRuntime.InvokeAsync<string>(
+            "exampleJsFunctions.showPrompt");
+    }
+}
--- demo-jsinterop/DemoJSInterop/Program.cs
+++ demo-jsinterop/DemoJSInterop/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace DemoChatJSInterop
+namespace DemoJSInterop
 {
     public class Program
     {
